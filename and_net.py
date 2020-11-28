fb1fe6fc0b7b56066a87478897d2c4e4fd9c2da5
iJaddu <Computers99!!>
2020-11-28 02:42:26 -0500

    added some raster plot functionality (still bugged)

--- and_net.py
+++ and_net.py
@@ -1,12 +1,14 @@
 from lif_neuron import lif_neuron
 from lif_connection import lif_connection
 
+
+
 class and_net:
 
     def __init__(self, neurons, connections):
-   
-        self.neurons = [] # A list of lif_neuron objects
-        self.connections = [] # A list of lif_connection objects
+
+        self.neurons = []  # A list of lif_neuron objects
+        self.connections = []  # A list of lif_connection objects
 
     # Function calculates current from an array of spikes from a neuron's output activity
     def spikes_to_current(self, a_spikes, conversion_factor):
@@ -15,7 +17,7 @@ class and_net:
         for i in range(len(a_spikes)):
             if a_spikes[i] == 1:
                 spike_count += 1
-        
+
         spikes_per_second = spike_count / len(a_spikes)
 
         return spikes_per_second * conversion_factor * 100000
@@ -27,7 +29,7 @@ class and_net:
         input_neuron_y = lif_neuron(id="input_neuron_y")
         teach_neuron = lif_neuron(id="teach_neuron")
         output_neuron = lif_neuron(id="output_neuron")
-        
+
         x_to_output = lif_connection(start=input_neuron_x, end=output_neuron, weight=1, id="x_to_output")
         y_to_output = lif_connection(start=input_neuron_y, end=output_neuron, weight=1, id="y_to_output")
         teach_to_output = lif_connection(start=teach_neuron, end=output_neuron, weight=1, id="teach_to_output")
@@ -39,7 +41,7 @@ class and_net:
 
     # Prepare an output of 1 or 0 based on the spike frequency of the output neuron
     def spikes_to_binary(self, output_neuron, threshold=4):
-        
+
         spike_count = 0
         for i in range(len(output_neuron.output)):
             if output_neuron.output[i] == 1:
@@ -47,11 +49,11 @@ class and_net:
 
         spike_frequency = spike_count / len(output_neuron.output)
 
-        if(spike_frequency > threshold):
+        if (spike_frequency > threshold):
             return 1
 
         return 0
-    
+
     # Converts a single bit into 1000 ms array of spikes
     # A value of 0 makes 5Hz spikes
     # A value of 1 makes 15Hz spikes
@@ -60,16 +62,16 @@ class and_net:
         a_spikes = [0] * nsteps
         nspikes = 5
 
-        if(val == 1):
+        if (val == 1):
             nspikes = 15
 
         interval = int(nsteps / nspikes)
         for i in range(1000):
-            if(i % interval == 0):
+            if (i % interval == 0):
                 a_spikes[i] = 1
 
         return a_spikes
-    
+
     # Runs the neural net
     # Maybe include option to run without training neuron
     def run_net(self, input):
@@ -93,3 +95,4 @@ class and_net:
             return 1
         else:
             return 0
+
--- training.py
+++ training.py
@@ -1,4 +1,6 @@
 from and_net import and_net
+import matplotlib.pyplot as plt
+
 net = and_net([], [])
 net.create_simple_net()
 # X at [0], Y at [1], Teacher at [2]
@@ -7,6 +9,42 @@ net.connections[1].weight = 1
 net.connections[2].weight = 1
 # data[0] holds X, data[1] holds Y, output calculated by data[0][i] && data[1][i]
 data = []
+i = 0
+j = 0
+k = 0
+
+
+def raster_plot(net):
+    plt.figure()
+    plt.title('Raster plot for network activity')
+
+    plt.subplot(3, 1, 1)
+    plt.xlabel('Time')
+    plt.ylabel('Input Neuron 1')
+    for i in range(1000):
+        if net.neurons[0].output[i] == 1:
+            fmt = '[|][-][k]'
+            plt.plot(fmt)
+
+    plt.subplot(3, 1, 2)
+    plt.xlabel('Time')
+    plt.ylabel('Input Neuron 2')
+    for j in range(1000):
+        if net.neurons[1].output[j] == 1:
+            fmt = '[|][-][k]'
+            plt.plot(fmt)
+
+    plt.subplot(3, 1, 3)
+    plt.xlabel('Time')
+    plt.ylabel('Output Neuron')
+    for k in range(1000):
+        if net.neurons[3].output[k] == 1:
+            fmt = '[|][-][k]'
+            plt.plot(fmt)
+
+    plt.show()
+
+
 #lines = open(input("Enter Filepath: "), "r").readlines()
 lines = open("data.txt", "r").readlines()
 runs = len(lines)
@@ -39,3 +77,4 @@ for r in range(runs):
     print("X =", net.connections[0].weight, x_spikes, "\nY =", net.connections[1].weight, y_spikes, "\n", o_spikes)
     for i in range(3):
         net.neurons[i].clear
+raster_plot(net)
